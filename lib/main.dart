faa5d09b02071d6aae25671437b689f2e6fbb736
Ahmed Salama <ahmed.salama@tigirsnet.com>
2023-02-28 15:59:34 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("xxxxxxxn"),
+        title: const Text("Welcome Salama to our world"),
       ),
       body: Center(
         child: Column(
